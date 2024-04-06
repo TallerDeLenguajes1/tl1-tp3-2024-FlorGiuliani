bfebd8859f49929d521cd869b3d38e5d2ab34866
FlorenciaGiuliani <florgiuliani5@gmail.com>
2024-04-06 11:56:19 -0300

    comencé el punto 3

---
+++ tp3_3.c
@@ -0,0 +1,15 @@
+/*3) Escriba un programa que solicite 5 nombres, los cargue en un vector de punteros char y una vez cargados sean listados por pantalla (Todo implementando reserva dinámica de memoria) (Lea las notas 1 y 2)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define CANT 5;
+
+int main()
+{
+    char ** nombres = (char **)malloc(sizeof(char*)* CANT );
+
+
+    return 0;
+}
