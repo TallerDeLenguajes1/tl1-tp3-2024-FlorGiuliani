65ea426c426845c4f05cb27374ff8aec6bb08df7
Giuliani Florencia <florgiuliani5@gmail.com>
2024-05-03 18:08:52 -0300

    Terminé el punto 3

--- tp3_3.c
+++ tp3_3.c
@@ -1,24 +1,29 @@
-/*3) Escriba un programa que solicite 5 nombres, los cargue en un vector de punteros char y una vez cargados sean listados por pantalla (Todo implementando reserva dinámica de memoria) (Lea las notas 1 y 2)
-*/
+/*3) Escriba un programa que solicite 5 nombres, los cargue en un vector de punteros char y una vez cargados sean listados por pantalla (Todo implementando reserva dinámica de memoria) (Lea las notas 1 y 2)*/
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdlib.h> //malloc
+#include <string.h> //strlen , strcpy
 
 int main()
 {
     int cant = 5;
-    char *Buff = (char *)malloc(100 * sizeof(char)); //variable auxiliar
     char **nombres = (char **)malloc(cant * sizeof(char*));
+    char *Buff = (char *)malloc(100 * sizeof(char)); //variable auxiliar 
 
     for (int i = 0; i < cant; i++)
     {
-        char *filaNombres[i] = (char *)malloc(100 * sizeof(char));
         printf("Ingrese un nombre: ");
         gets(Buff);
-        free(Buff);
+        nombres[i] = (char *)malloc(sizeof(char) * (strlen(Buff)+1));
+        strcpy(nombres[i], Buff);
     }
 
+    for (int i = 0; i < cant; i++)
+    {
+        printf("%s\n", *(nombres + i));
+    }
+
+    free(Buff);
     free(nombres);
     
     return 0;
